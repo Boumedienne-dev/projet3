62459f3b994f426d8939ecc6b3c6237c804e4ec4
Otire82 <a.jeanne01@sncf.fr>
2022-09-07 16:39:45 +0200

    test error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import AllLines from "@pages/AllLines";
 import ConnexionUser from "@pages/ConnexionUser";
 import RegistrationUser from "@pages/RegistrationUser";
 import AdminAccount from "@pages/AdminAccount";
-import Navbar from "./components/Navbar";
+import Navbar from "@components/Navbar";
 
 function App() {
   return (
