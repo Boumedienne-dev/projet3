bb0f5fb7e60db095e097a300ccf8d59568f4924d
Otire82 <a.jeanne01@sncf.fr>
2022-09-07 12:32:42 +0200

    error line3

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "@assets/style/Navbar.css";
+import "../assets/style/Navbar.css";
 
 export default function Navbar() {
   return (
