cc42de7f7c70a73537279e1531364ffc9e754cb2
Otire82 <a.jeanne01@sncf.fr>
2022-09-08 18:47:51 +0200

    debug TWO

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div>
-      <header>
-        <div className="">
-          <ul className="">
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </header>
+      <div className="">
+        <ul className="">
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
