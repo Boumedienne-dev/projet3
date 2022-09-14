081b2920272c58b8c982530195dbde096c226829
Dam116975 <76938706+Dam116975@users.noreply.github.com>
2022-09-14 14:19:37 +0200

    api installé et css fait

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LinePage from "@pages/LinePage";
+import AdminLinePage from "@pages/AdminLinePage";
 import Region from "./pages/Region";
 import AccountUser from "./pages/AccountUser";
 import Contact from "./pages/Contact";
@@ -25,6 +27,8 @@ function App() {
           <Route path="/region" element={<Region />} />
           <Route path="/compte_utilisateur" element={<AccountUser />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/line/:id" element={<LinePage />} />
+          <Route path="/admin/line" element={<AdminLinePage />} />
           <Route path="/ligne" element={<AllLines />} />
           <Route path="/ville" element={<City />} />
           <Route path="/utilisateur" element={<User />} />
