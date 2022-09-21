393efdd3bd856eb0c696ec03ca066525f190e157
remi6901 <descollonges.remi@gmail.com>
2022-09-21 14:59:40 +0200

    Show city by id_line

--- backend/src/models/CityManager.js
+++ backend/src/models/CityManager.js
@@ -7,7 +7,7 @@ class CityManager extends AbstractManager {
 
   findWithLineId(id) {
     return this.connection.query(
-      `select line_name, city_name from ${this.table} 
+      `select city_name from ${this.table} 
       inner join line_city on line_city.id_city=city.id
       inner join line on line_city.id_line=line.id WHERE line.id = ?`,
       [id]
@@ -16,15 +16,15 @@ class CityManager extends AbstractManager {
 
   insert(line) {
     return this.connection.query(
-      `insert into ${this.table} (id_region, line_name, picture) values (?, ?, ?)`,
-      [line.id_region, line.line_name, line.picture]
+      `insert into ${this.table} (city_name, description, picture) values (?, ?, ?)`,
+      [line.city_name, line.description, line.picture]
     );
   }
 
   update(line) {
     return this.connection.query(
-      `update ${this.table} set id_region = ?, descritpion = ?, picture = ? where id = ?`,
-      [line.id_region, line.line_name, line.picture, line.id]
+      `update ${this.table} set city_name = ?, descritpion = ?, picture = ? where id = ?`,
+      [line.city_name, line.description, line.picture, line.id]
     );
   }
 }
