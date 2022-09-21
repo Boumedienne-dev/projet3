80d4c59dfdd979dc80e7a809055e39ac7bfaf2b8
Dam116975 <76938706+Dam116975@users.noreply.github.com>
2022-09-21 11:15:53 +0200

    desktop

--- frontend/src/components/AllLinesList.jsx
+++ frontend/src/components/AllLinesList.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import "../assets/style/allLines.css";
 
 function AllLinesList({ line }) {
   return (
-    <div className="lineposflex">
+    <div className="">
       <div className="line-map">
         <div>
           <Link to={`/les_villes/${line.id}`}>
-            <div className="lineflex">
+            <div className="lineposflex">
               {line.picture ? (
                 <img
                   className="line-map-img"
