8465b2efeb1cc828a4ca0209c38b1bcacaa1c4b3
remi6901 <descollonges.remi@gmail.com>
2022-09-30 10:24:59 +0200

    add the method delete on favorite for account user

--- backend/src/controllers/userActivityControllers.js
+++ backend/src/controllers/userActivityControllers.js
@@ -16,6 +16,42 @@ const postUserActivity = (req, res) => {
     });
 };
 
+const destroy = (req, res) => {
+  models.user_activity
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroyActivityWithIdUser = (req, res) => {
+  const userActivity = req.body;
+
+  models.user_activity
+    .deleteActivityWithIdUser(userActivity)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   postUserActivity,
+  destroy,
+  destroyActivityWithIdUser,
 };
