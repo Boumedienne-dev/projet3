68ef2d6384c198900151291cbde0e60e61e7b8fe
Otire82 <a.jeanne01@sncf.fr>
2022-10-05 11:17:59 +0200

    prepared requestsql front Contact and Picture

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,50 @@
+import { axios } from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../assets/style/Contact.css";
 
 export default function Contact() {
+  const [comment, setComment] = useState({
+    lastName: "",
+    firstName: "",
+    mail: "",
+    comment: "",
+    picture: "",
+  });
+  const navigate = useNavigate();
+
+  const postComment = () => {
+    axios
+      .post(`${import.meta.env.VITE_BACKEND_URL}/comments`, {
+        ...comment,
+      })
+      .then((res) => {
+        console.error(res);
+        console.error(res.data);
+      })
+      .then(() => navigate("/"));
+  };
+
+  const uploadImage = (e) => {
+    const data = new FormData();
+    data.append("file", e.target.files[0]);
+    data.append("upload_preset", "sncf-exploration");
+    data.append("cloud_name", "otire82");
+    fetch("  https://api.cloudinary.com/v1_1/otire82/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data1) => {
+        setComment({ ...comment, picture: data1.url });
+      })
+      .catch((err) => console.error(err));
+  };
+
+  function handleInputProfile(e) {
+    uploadImage(e);
+    setComment({ ...comment, picture: e.target.value });
+  }
   return (
     <div className="ContactContainer">
       <h2 className="contactUs">Nous Contacter</h2>
@@ -8,16 +52,42 @@ export default function Contact() {
         Merci de nous laisser vos questions, commentaires ou suggestions en
         complétant ce formulaire.
       </p>
-      <form className="ContactForm">
+      <form
+        className="ContactForm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          postComment();
+        }}
+      >
         <p className="ContactRequired">*Champs obligatoires</p>
         <label className="ContactName" htmlFor="Nom" placeholder="Doe">
           Nom*
         </label>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          onChange={(e) =>
+            setComment({
+              ...comment,
+              lastName: e.target.value,
+            })
+          }
+          required
+        />
         <label className="ContactFirstName" htmlFor="prenom" placeholder="Jhon">
           Prenom*
         </label>
-        <input type="text" name="name" />
+        <input
+          type="text"
+          name="name"
+          onChange={(e) =>
+            setComment({
+              ...comment,
+              firstName: e.target.value,
+            })
+          }
+          required
+        />
         <label
           className="ContactEmail"
           htmlFor="email"
@@ -25,7 +95,17 @@ export default function Contact() {
         >
           Email*
         </label>
-        <input type="email" email="email" />
+        <input
+          type="email"
+          email="email"
+          onChange={(e) =>
+            setComment({
+              ...comment,
+              mail: e.target.value,
+            })
+          }
+          required
+        />
         <label
           className="ContactComment"
           htmlFor="message"
@@ -33,7 +113,33 @@ export default function Contact() {
         >
           Votre commentaire :*
         </label>
-        <input className="ContactTextarera" type="textarera" name="message" />
+        <input
+          className="ContactTextarera"
+          type="textarera"
+          name="message"
+          onChange={(e) =>
+            setComment({
+              ...comment,
+              comment: e.target.value,
+            })
+          }
+          required
+        />
+        <label className="ContactPicture" htmlFor="file">
+          Photo:
+        </label>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          alt="Photo"
+          accept="image/*"
+          onChange={(e) => handleInputProfile(e)}
+        />
+        <div className="ReviewContactPicture">
+          <p>Prévualisation de votre photo:</p>
+          <img alt={comment.picture} src={comment.picture} />
+        </div>
         <div className="ContactBtnPos">
           <button className="ContactToggleBlue" type="submit" value="Submit">
             <span className="ContactTextBlack">Valider</span>
